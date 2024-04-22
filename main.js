6d36d91f1b252ee36c032172f04f5e0ac6b1f498
Zachary-bungei <160045464+Zachary-bungei@users.noreply.github.com>
2024-04-22 20:32:24 +0300

    Add files via upload

---
+++ main.js
@@ -0,0 +1,14 @@
+const username = document.getElementById("use");
+const passward = document.getElementById("pass");
+function feedbact() {
+	if (passward === CheBung1132) {
+		window.alert("passward is correct");
+		if (username === zacharia.bungei) {
+			window.alert("username is correct");
+		}
+
+		} else {
+			window.alert("nothing correct");
+	}
+
+};
