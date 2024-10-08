93e09c555834004a1dfb49dd269f294ef2a67e63
Zachary-bungei <160045464+Zachary-bungei@users.noreply.github.com>
2024-10-08 20:56:36 +0300

    Update main.js

--- main.js
+++ main.js
@@ -1,13 +1,13 @@
 const username = document.getElementById("use");
 const passward = document.getElementById("pass");
 function feedbact() {
-	if (passward === "1234") {
+	if (passward == "1234") {
 		window.alert("passward is correct");
 		location.replace("https://calculateexponent.netlify.app");
 
 		} 
 	else {
-		window.alert("nothing correct");
+		window.alert("nothing correct from js");
 	}
 
 };
